d20c284c4597fe48a734c3e56640b5e0ff82360f
michał kurecki <Michal.Kurecki@outlook.com>
2023-06-07 22:49:05 +0200

    Remove Exception from Demo

--- Library/Program.cs
+++ Library/Program.cs
@@ -3,12 +3,6 @@ using Library.Logic;
 
 Console.WriteLine("Hello, World!");
 RentalLogic.RentBook(12345678, Guid.Parse("0cbc19bc-0882-4572-8f2f-b5919b303bed"));
-RentalLogic.RentBook(12345678, Guid.Parse("0cbc19bc-0882-4572-8f2f-b5919b303bed"));
-
-RentalLogic.RentBook(12345678, Guid.Parse("0cbc19bc-0882-4572-8f2f-b5919b303bed"));
-
-RentalLogic.RentBook(12345678, Guid.Parse("0cbc19bc-0882-4572-8f2f-b5919b303bed"));
-
-RentalLogic.RentBook(12345678, Guid.Parse("0cbc19bc-0882-4572-8f2f-b5919b303bed"));
+RentalLogic.RentBook(42345678, Guid.Parse("0cbc19bc-0882-4572-8f2f-b5919b303bed"));
 
 RentalLogic.EndRental(12345678, Guid.Parse("0cbc19bc-0882-4572-8f2f-b5919b303bed"));
